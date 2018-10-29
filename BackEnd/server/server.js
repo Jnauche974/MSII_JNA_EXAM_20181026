397a7947497e85c9b2792b444eab9cb8cfb8d0ee
NAUCHE Julien <julien.nauche@srr.fr>
2018-10-29 09:07:03 +0400

    essaye modifier fichier BackEnd

--- BackEnd/server/server.js
+++ BackEnd/server/server.js
@@ -24,6 +24,40 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) {
+    app.io = require('socket.io')(app.start());
+    // Use this with user authentification
+
+    // app.subscribers = [];
+    // app.io.on('connection', function(socket) {
+    //   console.info('client connected');
+    //   socket.on('disconnect', function() {
+    //     console.info('client disconnected');
+    //   });
+    //   socket.on('subscribe', function(payload) {
+    //     const topic = payload.toString();
+    //     if (!app.subscribers[topic]) {
+    //       app.subscribers[topic] = [];
+    //     }
+    //     const isAlreadySub = app.subscribers[topic]
+    //       .some((soc) => { return soc.id === socket.id; });
+    //     if (!isAlreadySub) {
+    //       app.subscribers[topic].push(socket);
+    //       app.mqttClient.subscribe(topic);
+    //     }
+    //   });
+
+    //   socket.on('unsubscribe', function(payload) {
+    //     const topic = payload.toString();
+    //     console.info(`try to unsubscribe topic: ${topic}`);
+    //     if (app.subscribers[topic]) {
+    //       app.subscribers[topic] = app.subscribers[topic]
+    //       .filter((soc) => {
+    //         return soc.id !== socket.id;
+    //       });
+    //       app.mqttClient.unsubscribe(topic);
+    //     }
+    //   });
+    // });
+  }
 });
